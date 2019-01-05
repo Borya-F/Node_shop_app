8c8dff3f20c91221da79cfe3afc0f14b08debcf7
Borya Fishman <boryafishman@gmail.com>
2019-01-05 10:59:32 -0500

    cart item deletion implemented

--- controllers/shop_controller.js
+++ controllers/shop_controller.js
@@ -1,89 +1,96 @@
 const Product = require('../models/product.js');
 const Cart = require('../models/cart.js');
 
-exports.getHome = (req,res,next) => {
-	
-	Product.fetchAllProducts()
-	.then(products=>{
-		res.render('shop/shop',{
-			activeNav: "home",
-			pageTitle: "home",
-			prods: products
-		});
-	})
-	.catch(err=>{
-		throw err;
-	});
+exports.getHome = (req, res, next) => {
+
+    Product.fetchAllProducts()
+        .then(products => {
+            res.render('shop/shop', {
+                activeNav: "home",
+                pageTitle: "home",
+                prods: products
+            });
+        })
+        .catch(err => {
+            throw err;
+        });
 };
 
-exports.getProductDetail = (req,res,next) => {
+exports.getProductDetail = (req, res, next) => {
 
-	const prodId = req.params.id;
+    const prodId = req.params.id;
 
-	Product.fetchProductById(prodId)
-	.then(prod=>{
-		res.render('shop/product_detail',{
-			pageTitle: prod.title,
-			product: prod
-		});
-	});
+    Product.fetchProductById(prodId)
+        .then(prod => {
+            res.render('shop/product_detail', {
+                pageTitle: prod.title,
+                product: prod
+            });
+        });
 }
 
-exports.getCart = (req,res,next) => {
+exports.getCart = (req, res, next) => {
 
-	let cartToSend;
+    let cartToSend;
 
-	Cart.fetchCartFileContent()
-	.then(cart=>{
-		cartToSend = {...cart};
-		return Product.fetchAllProducts();
-	})
-	.then(products=>{
+    Cart.fetchCartFileContent()
+        .then(cart => {
+            cartToSend = { ...cart };
+            return Product.fetchAllProducts();
+        })
+        .then(products => {
 
-		const currentItems = cartToSend.cartProducts.map(el=>el.itemId);
-		let prodsArray = products.filter(prod=>currentItems.includes(prod.id));
-		prodsArray = prodsArray.map(prod=>{
-			//find item in cartToSend with the same id as prod
-			//attach qty from that item to prodsArray.
-			
-			let item = cartToSend.cartProducts.find(el=>el.itemId === prod.id);
-			let itemToReturn = {...prod, qty:item.qty};
-			
-			return itemToReturn;
-		});
+            const currentItems = cartToSend.cartProducts.map(el => el.itemId);
+            let prodsArray = products.filter(prod => currentItems.includes(prod.id));
+            prodsArray = prodsArray.map(prod => {
+                //find item in cartToSend with the same id as prod
+                //attach qty from that item to prodsArray.
 
-		res.render('shop/cart',{
-			pageTitle: "cart",
-			activeNav: "cart",
-			price: cartToSend.totalPrice,
-			prods: prodsArray
-		})
-	})
-	.catch(err=>{
-		throw err;
-	});
+                let item = cartToSend.cartProducts.find(el => el.itemId === prod.id);
+                let itemToReturn = { ...prod, qty: item.qty };
+
+                return itemToReturn;
+            });
+
+            res.render('shop/cart', {
+                pageTitle: "cart",
+                activeNav: "cart",
+                price: cartToSend.totalPrice,
+                prods: prodsArray
+            })
+        })
+        .catch(err => {
+            throw err;
+        });
 };
 
-exports.postAddToCart = (req,res,next) => {
-	const itemToAdd = req.body.productId;
-	const itemPrice = req.body.productPrice;
+exports.postCartDelItem = (req, res, next) => {
 
-	Cart.addItemToCart(itemToAdd,itemPrice)
-	.then(msg=>{
-		console.log(msg);
-		return res.redirect('/home');
-	})
-	.catch(err=>{
-		console.log(err);
-	});
+    const itemId = req.body.prodId;
+    const itemPrice = req.body.prodPrice;
+
+    Cart.removeProductFromCart(itemId,itemPrice)
+    .then(msg=>{
+    	res.redirect('/cart');
+    })
+    .catch(err=>{
+    	console.log(err);
+    })
+
+
+    
+}
+
+exports.postAddToCart = (req, res, next) => {
+    const itemToAdd = req.body.productId;
+    const itemPrice = req.body.productPrice;
+
+    Cart.addItemToCart(itemToAdd, itemPrice)
+        .then(msg => {
+            console.log(msg);
+            return res.redirect('/home');
+        })
+        .catch(err => {
+            console.log(err);
+        });
 };
-
-
-
-
-
-
-
-
-
-
