e8f3e2e639e9aaddc5409c605138edf93b8ad79a
Borya Fishman <boryafishman@gmail.com>
2019-01-15 15:16:57 -0500

    cleanup

--- controllers/shop_controller.js
+++ controllers/shop_controller.js
@@ -6,7 +6,6 @@ const msg = require('../util/messagelog.js');
 
 exports.getHome = (req, res, next) => {
 
-    msg.test(process.pid,'shopcntl');
     Product.fetchAll()
     .then(products=>{
         res.render('shop/shop',{
